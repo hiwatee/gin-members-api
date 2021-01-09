184d3fbb020fa633519523a85bfab6af1ece3670
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2021-01-09 16:14:10 +0900

    feat: update swagger settings

--- main.go
+++ main.go
@@ -5,6 +5,20 @@ import (
 	"members/server"
 )
 
+// @title members-api
+// @version バージョン(1.0)
+// @description 仕様書に関する内容説明
+// @termsOfService 仕様書使用する際の注意事項
+
+// @contact.name APIサポーター
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name ライセンス(必須)
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
 func main() {
 	db.Init()
 	server.Init()
