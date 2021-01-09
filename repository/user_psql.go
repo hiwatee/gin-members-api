69e3197d04023899a178b1a037570bc2b86c8d73
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2021-01-09 15:38:28 +0900

    feat: update api

--- repository/user_psql.go
+++ repository/user_psql.go
@@ -14,15 +14,15 @@ type UserRepository struct{}
 type User models.User
 
 type UserProfile struct {
-	Name string
-	Id   int
+	Id    int
+	Email string
 }
 
 // GetAll is get all User
 func (_ UserRepository) GetAll() ([]UserProfile, error) {
 	db := db.GetDB()
 	var u []UserProfile
-	if err := db.Table("users").Select("name, id").Scan(&u).Error; err != nil {
+	if err := db.Table("users").Select("id, email").Scan(&u).Error; err != nil {
 		return nil, err
 	}
 	return u, nil
