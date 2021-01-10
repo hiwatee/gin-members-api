7eda1077ad13b93adb5b988d6864937224a641b9
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2021-01-10 22:45:21 +0900

    feat: add auto migrations

--- migration/migration.go
+++ migration/migration.go
@@ -5,8 +5,11 @@ import (
 	"members/models"
 )
 
+// AutoMigration ...
 func AutoMigration() {
 	database := db.GetDB()
 	database.AutoMigrate(&models.User{})
 	database.AutoMigrate(&models.Profile{})
+	database.AutoMigrate(&models.Token{})
+	database.AutoMigrate(&models.AccessToken{})
 }
