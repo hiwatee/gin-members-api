be9834cd4bcf7c01e59381edbc816033ec2c03d7
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2021-01-11 15:39:27 +0900

    feat: fix loginMethod

--- controllers/login.go
+++ controllers/login.go
@@ -57,6 +57,9 @@ func (pc LoginController) Create(c *gin.Context) {
 	log.Print(token)
 	log.Print(accessToken)
 
-	c.JSON(201, gin.H{"message": "login_success", "token": "this is acccess_token"})
+	c.SetCookie("token", token, 60*60*24*30, "/", "", false, false)
+	c.SetCookie("access_token", token, 60*60*2*30, "/", "", false, false)
+
+	c.JSON(201, gin.H{"message": "login_success", "token": accessToken})
 	return
 }
